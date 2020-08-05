7a5874d1ff8ab8479bc34473d9649aef72bbea5e
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-05 09:05:56 -0700

    add endpoint

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require("express");
-const morgan = require("morgan");
-
-const app = express();
-
-app.use(morgan("dev"));
-
-app.use((req, res) => {
-  res.send("Hello, word!");
-});
-
+const app = require("./app");
 const PORT = 8000;
 
 app.listen(PORT, () => {
