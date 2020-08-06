0ad32c04b8fc1a427887275a09e5429d4b3a85be
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-06 07:19:40 -0700

    implement endpoint with API token validation
    Used a hack to require an API token for the purposes of learning
    server security concepts.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-
+require("dotenv").config();
 const app = express();
 
 const movies = [
@@ -11,8 +11,20 @@ const movies = [
 
 app.use(morgan("dev"));
 
+app.use(function validateBearerToken(req, res, next) {
+  console.log("validate bearer token middleware");
+  const authToken = req.get("Authorization");
+  const apiToken = process.env.API_TOKEN;
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  // move to the next middleware
+  next();
+});
+
 function handleGetReq(req, res) {
-  res.json(movies).send();
+  res.send(movies);
 }
 
 app.get("/movies", handleGetReq);
