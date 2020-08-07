8c16e34b2c19c70e6fb6596bb5368fe636befbf4
Amy Pepper <amyandmicahpepper@gmail.com>
2020-08-07 08:17:27 -0700

    prepare app for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
